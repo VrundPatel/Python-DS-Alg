80d5833fa79186bc3614129d7e4a27d083548992
Vrund Patel <vrundp07@yahoo.com>
2016-10-06 22:24:51 -0400

    Simple array example.

---
+++ arrays.py
@@ -0,0 +1,13 @@
+nums = [1,2,3,4,5]
+
+# print the whole array
+print(nums)
+
+# O(1) if we know the index
+print(nums[0])
+
+# print a range of elements
+print(nums[1:3])
+
+# Do not print the last two elements
+print(nums[:-2])
